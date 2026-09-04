602d4cdfe6c5d2808fbbae6ddf17c4a261c9f6b5
Abduletif Abdella <abduletifabdella29@gmail.com>
2026-09-04 11:14:00 +0300

    feat: conversations save to database

--- 01. AI Full-Stack Web Developer Course/02. AI Powered Application Architecture & Building the Chat Bot/Chat-GPT Clone/Backend/src/api/chat/service/chat.service.js
+++ 01. AI Full-Stack Web Developer Course/02. AI Powered Application Architecture & Building the Chat Bot/Chat-GPT Clone/Backend/src/api/chat/service/chat.service.js
@@ -1,12 +1,20 @@
+import { Connection } from 'mysql2';
+import db from '../../../../db/db.config.js'
+
 export async function createConvesatioService(question) {
     try {
-
         // validation
         if (!question.trim()) {
             const error = new Error('Question is requierd')
             error.status = 400;
             throw error;
         }
+
+        // save to db
+        await db.execute(
+            'INSERT INTO conversations (content) VALUE (?)',
+            [question,]
+        );
 
         return `chat saved to db with question: ${question}`;
     } catch (error) {
