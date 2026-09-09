8e8ac9e32517f62f97ed507f957f5d9692f3371b
Abduletif Abdella <abduletifabdella29@gmail.com>
2026-09-09 15:07:29 +0300

    feat(frontend): implement ChatHeader component and styles - Add ChatHeader component featuring model selection dropdown trigger and user avatar - Create CSS module rules for header flex layout, typography, and interactive spacing

--- 01. AI Full-Stack Web Developer Course/02-AI-Powered-Application-Architecture-and-Building-the-Chat-Bot/Chat-GPT Clone/Frontend/src/App.jsx
+++ 01. AI Full-Stack Web Developer Course/02-AI-Powered-Application-Architecture-and-Building-the-Chat-Bot/Chat-GPT Clone/Frontend/src/App.jsx
@@ -1,10 +1,14 @@
-import Sidebar from './Componentes/Sidebar/Sidebar';
 import './App.css';
+import Sidebar from './Componentes/Sidebar/Sidebar';
+import ChatHeader from './Componentes/ChatHeader/ChatHeader'
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
+      <main className='main'>
+        <ChatHeader />
+      </main>
     </div>
   );
 }
