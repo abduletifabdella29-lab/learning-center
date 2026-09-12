bcf4c8fef6a85ee9e81b110938fbe1058a38450d
Abduletif Abdella <abduletifabdella29@gmail.com>
2026-09-12 11:34:15 +0300

    fix: updates some error

--- 01. AI Full-Stack Web Developer Course/02-AI-Powered-Application-Architecture-and-Building-the-Chat-Bot/Chat-GPT Clone/Backend/src/api/chat/chat.routes.js
+++ 01. AI Full-Stack Web Developer Course/02-AI-Powered-Application-Architecture-and-Building-the-Chat-Bot/Chat-GPT Clone/Backend/src/api/chat/chat.routes.js
@@ -4,10 +4,8 @@ const chatRouter = express.Router();
 
 import { createConvesationController, getConvesationController } from './contoller/chat.controller.js'
 
-// api/chat/conversations (በብዙ ቁጥር)
 chatRouter.post('/conversation',  createConvesationController);
 
-// 'conversations' ብለህ አስተካክለው (s ጨምርበት)
 chatRouter.get('/conversations', getConvesationController);
 
 export default chatRouter
